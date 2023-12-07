b9672acbe4d1bb32fbe6c9be6986b05acf642bb9
arumugarajajo <arumugarajajo@gmail.com>
2023-12-07 21:34:59 +0530

    grid-qtify-takehome

--- src/components/cardComponents/CardComponents.jsx
+++ src/components/cardComponents/CardComponents.jsx
@@ -1,23 +1,22 @@
 import React, { useState, useEffect } from "react";
 import "./CardComponents.css";
-import { FaChevronLeft, FaChevronRight } from 'react-icons/fa'; // Import arrow icons
-
+import Slider from "react-slick";
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 function CardComponents() {
   const [albums, setAlbums] = useState([]);
   const [displayedAlbums, setDisplayedAlbums] = useState(7);
   const albumsPerPage = 7;
 
   useEffect(() => {
-    // Fetching data from the provided API
     fetch("https://qtify-backend-labs.crio.do/albums/top")
       .then((response) => response.json())
       .then((data) => setAlbums(data));
   }, []);
 
-  const showMoreAlbums = (increment) => {
-    const newDisplayedAlbums = Math.min(albums.length, displayedAlbums + increment);
-    setDisplayedAlbums(newDisplayedAlbums);
-  };
+  // const showMoreAlbums = (increment) => {
+  //   const newDisplayedAlbums = Math.min(albums.length, displayedAlbums + increment);
+  //   setDisplayedAlbums(newDisplayedAlbums);
+  // };
 
   const showPreviousAlbums = () => {
     const newDisplayedAlbums = Math.max(0, displayedAlbums - albumsPerPage);
@@ -29,43 +28,46 @@ function CardComponents() {
     setDisplayedAlbums(newDisplayedAlbums);
   };
 
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,  
+    slidesToShow: 1, 
+    slidesToScroll: 1, 
+    vertical: false,
+    prevArrow: <FaChevronLeft className="arrow left" onClick={showPreviousAlbums} />,
+    nextArrow: <FaChevronRight className="arrow right" onClick={showNextAlbums} />,
+  };
   return (
-    <div className="cardComponents">
-      <h3>Top Albums</h3>
-      <div className="card-container">
-        {albums.slice(0, displayedAlbums).map((album, index) => (
-          <div className="card" key={index}>
-            {/* Card content */}
-            <div className="inside">
-              <div className="top">
-                <div className="img-wrapper">
-                  <img
-                    className="img"
-                    src={album.imageUrl}
-                    alt="Album Cover"
-                  />
-                  <div className="title">{album.title}</div>
-                  <div className="year">{album.year}</div>
+    <div className="slider-container">
+    <Slider {...settings}>
+      <div className="cardComponents">
+        
+        <h3>Top Albums</h3>
+        <div className="card-container">
+          {albums.slice(0, displayedAlbums).map((album, index) => (
+            <div className="card" key={index}>
+              <div className="inside">
+                <div className="top">
+                  <div className="img-wrapper">
+                    <img
+                      className="img"
+                      src={album.image}
+                      alt="Album Cover"
+                    />
+                    <div className="title">{album.title}</div>
+                    <div className="description">{album.description}</div>
+                  </div>
+                  <div className="follows">{album.follows} Follows</div>
                 </div>
-                <div className="follows">{album.follows}</div>
+                <div className="slug">{album.slug}</div>
               </div>
-              <div className="slug">{album.slug}</div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-      <div className="navigation">
-        <button onClick={showPreviousAlbums} disabled={displayedAlbums <= albumsPerPage}>
-          <FaChevronLeft />
-        </button>
-        {albums.length > displayedAlbums && (
-          <button onClick={() => showMoreAlbums(albumsPerPage)}>See More</button>
-        )}
-        <button onClick={showNextAlbums} disabled={displayedAlbums >= albums.length}>
-          <FaChevronRight />
-        </button>
-      </div>
-    </div>
+    </Slider>
+    </div>  
   );
 }
 
