cab00074232faff5437fdfc4481e2fd203646325
arumugarajajo <arumugarajajo@gmail.com>
2023-12-07 22:12:50 +0530

    grid-qtify-takehome

--- src/components/cardComponents/CardComponents.jsx
+++ src/components/cardComponents/CardComponents.jsx
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from "react";
 import "./CardComponents.css";
-import Slider from "react-slick";
 import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
+
 function CardComponents() {
   const [albums, setAlbums] = useState([]);
-  const [displayedAlbums, setDisplayedAlbums] = useState(7);
   const albumsPerPage = 7;
 
   useEffect(() => {
@@ -13,61 +12,34 @@ function CardComponents() {
       .then((data) => setAlbums(data));
   }, []);
 
-  // const showMoreAlbums = (increment) => {
-  //   const newDisplayedAlbums = Math.min(albums.length, displayedAlbums + increment);
-  //   setDisplayedAlbums(newDisplayedAlbums);
-  // };
-
-  const showPreviousAlbums = () => {
-    const newDisplayedAlbums = Math.max(0, displayedAlbums - albumsPerPage);
-    setDisplayedAlbums(newDisplayedAlbums);
-  };
-
-  const showNextAlbums = () => {
-    const newDisplayedAlbums = Math.min(albums.length, displayedAlbums + albumsPerPage);
-    setDisplayedAlbums(newDisplayedAlbums);
-  };
-
-  var settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,  
-    slidesToShow: 1, 
-    slidesToScroll: 1, 
-    vertical: false,
-    prevArrow: <FaChevronLeft className="arrow left" onClick={showPreviousAlbums} />,
-    nextArrow: <FaChevronRight className="arrow right" onClick={showNextAlbums} />,
-  };
   return (
-    <div className="slider-container">
-    <Slider {...settings}>
-      <div className="cardComponents">
-        
-        <h3>Top Albums</h3>
-        <div className="card-container">
-          {albums.slice(0, displayedAlbums).map((album, index) => (
-            <div className="card" key={index}>
-              <div className="inside">
-                <div className="top">
-                  <div className="img-wrapper">
-                    <img
-                      className="img"
-                      src={album.image}
-                      alt="Album Cover"
-                    />
-                    <div className="title">{album.title}</div>
-                    <div className="description">{album.description}</div>
-                  </div>
-                  <div className="follows">{album.follows} Follows</div>
-                </div>
-                <div className="slug">{album.slug}</div>
-              </div>
-            </div>
-          ))}
-        </div>
+    <div className="cardComponents">
+      <div className="top-text">
+      <h3>Top Albums</h3>
+      <h3>Select All</h3>
       </div>
-    </Slider>
-    </div>  
+      <div className="card-container">
+        {albums.map((album, index) => (
+          <div className="card" key={index}>
+            <div className="inside">
+              <div className="top">
+                <div className="img-wrapper">
+                  <img
+                    className="img"
+                    src={album.image}
+                    alt="Album Cover"
+                  />
+                  <div className="title">{album.title}</div>
+                  <div className="description">{album.description}</div>
+                </div>
+                <div className="follows">{album.follows} Follows</div>
+              </div>
+              <div className="slug">{album.slug}</div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
 
