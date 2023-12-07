20b556c3ff635ca1c5252e0642010baafd8feba9
arumugarajajo <arumugarajajo@gmail.com>
2023-12-07 12:31:42 +0530

    Top songs on progress

--- src/components/cardComponents/CardComponents.jsx
+++ src/components/cardComponents/CardComponents.jsx
@@ -1,77 +1,69 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./CardComponents.css";
-import cardImage from "../../assets/Group 7479.png";
-import { Carousel } from "react-bootstrap";
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa'; // Import arrow icons
+
 function CardComponents() {
+  const [albums, setAlbums] = useState([]);
+  const [displayedAlbums, setDisplayedAlbums] = useState(7);
+  const albumsPerPage = 7;
+
+  useEffect(() => {
+    // Fetching data from the provided API
+    fetch("https://qtify-backend-labs.crio.do/albums/top")
+      .then((response) => response.json())
+      .then((data) => setAlbums(data));
+  }, []);
+
+  const showMoreAlbums = (increment) => {
+    const newDisplayedAlbums = Math.min(albums.length, displayedAlbums + increment);
+    setDisplayedAlbums(newDisplayedAlbums);
+  };
+
+  const showPreviousAlbums = () => {
+    const newDisplayedAlbums = Math.max(0, displayedAlbums - albumsPerPage);
+    setDisplayedAlbums(newDisplayedAlbums);
+  };
+
+  const showNextAlbums = () => {
+    const newDisplayedAlbums = Math.min(albums.length, displayedAlbums + albumsPerPage);
+    setDisplayedAlbums(newDisplayedAlbums);
+  };
+
   return (
     <div className="cardComponents">
+      <h3>Top Albums</h3>
       <div className="card-container">
-        <Carousel className="carousel slide" data-ride="carousel">
-          {" "}
-          <Carousel.Item>
-            <img
-              className="carousel-image img-fluid"
-              src={cardImage}
-              alt="First slide"
-            />
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="carousel-image img-fluid"
-              src={cardImage}
-              alt="Second slide"
-            />
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="carousel-image img-fluid"
-              src={cardImage}
-              alt="Second slide"
-            />
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="carousel-image img-fluid"
-              src={cardImage}
-              alt="Second slide"
-            />
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="carousel-image img-fluid"
-              src={cardImage}
-              alt="Second slide"
-            />
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="carousel-image img-fluid"
-              src={cardImage}
-              alt="Second slide"
-            />
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="carousel-image img-fluid"
-              src={cardImage}
-              alt="Second slide"
-            />
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="carousel-image img-fluid"
-              src={cardImage}
-              alt="Second slide"
-            />
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="carousel-image img-fluid"
-              src={cardImage}
-              alt="Second slide"
-            />
-          </Carousel.Item>
-        </Carousel>
+        {albums.slice(0, displayedAlbums).map((album, index) => (
+          <div className="card" key={index}>
+            {/* Card content */}
+            <div className="inside">
+              <div className="top">
+                <div className="img-wrapper">
+                  <img
+                    className="img"
+                    src={album.imageUrl}
+                    alt="Album Cover"
+                  />
+                  <div className="title">{album.title}</div>
+                  <div className="year">{album.year}</div>
+                </div>
+                <div className="follows">{album.follows}</div>
+              </div>
+              <div className="slug">{album.slug}</div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="navigation">
+        <button onClick={showPreviousAlbums} disabled={displayedAlbums <= albumsPerPage}>
+          <FaChevronLeft />
+        </button>
+        {albums.length > displayedAlbums && (
+          <button onClick={() => showMoreAlbums(albumsPerPage)}>See More</button>
+        )}
+        <button onClick={showNextAlbums} disabled={displayedAlbums >= albums.length}>
+          <FaChevronRight />
+        </button>
       </div>
     </div>
   );
